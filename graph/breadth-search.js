7e668c9ddbd8a5b35efa4169a44a60b54f90e6bc
Oleg <akimba@gmail.com>
2022-08-31 14:44:35 +0300

    Добавляет алгоритм поиска в ширину в графе

---
+++ graph/breadth-search.js
@@ -0,0 +1,31 @@
+// Особенность поиска в шириру в том,
+// что сначала просматриваются все элементы на одном уровне вложенности
+// и, если искомый элемент не найден, поиск переходит на ступень ниже.
+
+const graph = {};
+
+graph.a = ['b', 'c'];
+graph.b =['f'];
+graph.c=['d', 'e'];
+graph.d = ['f'];
+graph.e = ['f'];
+graph.f = ['g'];
+
+function breadthSearch(graph, start, end) {
+  const queue = [];
+  queue.push(start);
+  while (queue.length) {
+    const currentNode = queue.shift();
+    if (!graph[currentNode]) {
+      graph[currentNode] = [];
+    }
+    if (currentNode.includes(end)) {
+      return true;
+    }
+    queue.push(...graph[currentNode]);
+  }
+  return false;
+}
+
+const result = breadthSearch(graph, 'a', 'g');
+console.log(result);
